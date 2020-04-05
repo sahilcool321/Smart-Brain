98f6c5bfb9dbe9e727c28544f79da2caa6b8e488
sahilcool321 <56647626+Rohitsharma-cool@users.noreply.github.com>
2020-04-05 20:30:28 +0530

    final addition

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import './App.css';
 const particlesOptions = {
   particles: {
     number: {
-      value: 160,
+      value: 165,
       density: {
         enable: true,
         value_area: 800
