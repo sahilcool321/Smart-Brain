dd3cc9625c917e6c11a0960ccdb24a68261496a4
sahilcool321 <56647626+Rohitsharma-cool@users.noreply.github.com>
2020-04-05 18:21:17 +0530

    soe

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://agile-dawn-90769.herokuapp.com/register', {
+    fetch('https://agile-dawn-90769.herokuapp.com/register',      
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
